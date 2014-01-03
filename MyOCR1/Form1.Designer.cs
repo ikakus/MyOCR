285de29b57a39e50f795b06d703401aa3bc59d88
ikakus <idadiani88@gmail.com>
2014-01-03 22:50:28 +0400

    first commit

---
+++ MyOCR1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MyOCR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox_matrix = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_matrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(85, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(316, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 371);
+            this.textBox1.TabIndex = 3;
+            // 
+            // pictureBox_matrix
+            // 
+            this.pictureBox_matrix.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox_matrix.Location = new System.Drawing.Point(130, 12);
+            this.pictureBox_matrix.Name = "pictureBox_matrix";
+            this.pictureBox_matrix.Size = new System.Drawing.Size(40, 50);
+            this.pictureBox_matrix.TabIndex = 4;
+            this.pictureBox_matrix.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 395);
+            this.Controls.Add(this.pictureBox_matrix);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_matrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox_matrix;
+    }
+}
+
---
+++ MyOCR1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace MyOCR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap pict;
+        MyOCRClass2 mOcr;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mOcr = new MyOCRClass2();
+            mOcr.fillEtalonBase();
+        }
+
+        private void open_Image()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+
+                try
+                {
+
+                    // create image document
+                    pict = new Bitmap(ofd.FileName);
+                    this.Text = ofd.FileName;
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+
+                }
+                catch (ApplicationException ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+            }		
+
+        }
+
+
+        private Bitmap draw_Matrix(MyOCRClass2.Matrix m)
+        {
+            Bitmap bmp = new Bitmap(30, 40);
+
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    //if (m.matrix[i, j] == 1)
+                    //{
+                    //    bmp.SetPixel(i, j, Color.White);
+                    //}
+
+                    if (m.matrix[i, j] == 0)
+                    {
+                        bmp.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+            return bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            open_Image();
+
+           mOcr.compareMatrix(mOcr.createMatrix(pict));
+
+           // ResizeNearestNeighbor Resizefilter = new ResizeNearestNeighbor(60, 70);
+            //Bitmap resized = Resizefilter.Apply(draw_Matrix(mOcr.createMatrix(pict)));
+           // pictureBox_matrix.Image = draw_Matrix(mOcr.createMatrix(pict));
+            label1.Text = (mOcr.getLetter());
+
+           
+        }
+
+
+
+
+
+    }
+}
